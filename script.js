09d529f141b8996a9e2507fbbe36d75ef2a2441c
agad212 <125690276+agad212@users.noreply.github.com>
2023-02-28 03:16:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-<script src="script.js"></script>
+// Load the Excel file using SheetJS
+var file = 'https://your-cloud-storage-service.com/your-excel-file.xlsx';
+var workbook = XLSX.readFile(file);
+
+// Get the worksheet that contains your data
+var sheet_name = workbook.SheetNames[0];
+var worksheet = workbook.Sheets[sheet_name];
+
+// Get the input values from the form
+var input1 = document.getElementById('input1').value;
+var input2 = document.getElementById('input2').value;
+
+// Use SheetJS to read the data from the Excel file and perform calculations
+var result = worksheet['A1'].v + input1 + input2;
+
+// Display the result on the website
+document.getElementById('result').innerHTML = result;
